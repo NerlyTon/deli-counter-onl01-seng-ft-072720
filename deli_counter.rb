99fb9eeeafd76734646d2216aa4cc5da96f83197
Nerly Ton <nerlyton@gmail.com>
2020-07-30 22:10:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,9 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else katz_deli.each do |name|
-    katz_deli.push()
+    katz_deli.push("#{name}")
+    count += 1
+  end
+  puts 
  end
 end

64cfb7f66a2142ec6a7aae3220a9e8e4036c9c10
Nerly Ton <nerlyton@gmail.com>
2020-07-30 23:46:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,6 +11,6 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, person)
-  katz_deli.push(#{person})
+  katz_deli.push(person)
   puts "Welcome, #{person}. You are number #{index} in line."
 end

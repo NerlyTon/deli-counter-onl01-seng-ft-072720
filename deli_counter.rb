1d5f4caba5f965e66fdb63fe866f2fe427d2fcc2
Nerly Ton <nerlyton@gmail.com>
2020-07-30 23:00:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ def line(katz_deli)
     puts "The line is currently empty."
   else 
     katz_deli.each_with_index do |name, index|
-   "The line is currently: #{index + 1}. #{name}"
-  
+   katz_deli.push (#{index + 1}. #{name}")
+  end
+  puts "The line is currently: katz_deli.join("")"
   end
  end
-end
